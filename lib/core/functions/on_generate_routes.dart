efff304889c4f1bf4280d146523a6289076e4cc1
Muhamad-Hasan <460995mhmdhsn@gmail.com>
2025-09-18 17:47:27 +0300

    first commit

---
+++ lib/core/functions/on_generate_routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_shop/features/on_boarding/presentation/view/on_boarding_view.dart';
+import 'package:fruit_shop/features/splash/presentation/view/splash_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
